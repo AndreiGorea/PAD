d702fe00f63999b75ef461a1ce57e8baa3435c23
Gorea Andrei <andrei.gorya@mail.ru>
2023-09-25 00:21:02 +0300

    Uploading the first part of the first part of the first lab

---
+++ Broker/Program.cs
@@ -0,0 +1,22 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Broker
+{
+     internal class Program
+     {
+          static void Main(string[] args)
+          {
+               Console.WriteLine("Broker");
+
+               BrokerSocket socket = new BrokerSocket();
+               socket.Start(Settings.BROKER_IP, Settings.BROKER_PORT);
+
+               Console.ReadLine();
+          }
+     }
+}
---
+++ Publisher/Program.cs
@@ -0,0 +1,44 @@
+﻿using Common;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publisher
+{
+     internal class Program
+     {
+          static void Main(string[] args)
+          {
+               Console.WriteLine("Publisher");
+
+               var publisherSocket = new PublisherSocket();
+
+               publisherSocket.Connect(Settings.BROKER_IP, Settings.BROKER_PORT);
+
+               if (publisherSocket.IsConnected)
+               {
+                    while (true)
+                    {
+                         var payload = new Payload();
+
+                         Console.WriteLine("Enter the topic: ");
+                         payload.Topic = Console.ReadLine().ToLower();
+
+                         Console.WriteLine("Enter the message: ");
+                         payload.Message = Console.ReadLine();
+
+                         var payloadString = JsonConvert.SerializeObject(payload); // serializare transformare in json
+                         byte[] data = Encoding.UTF8.GetBytes(payloadString);
+
+                         publisherSocket.Send(data);
+                    }
+               }
+
+               Console.ReadLine();
+          }
+     }
+}
